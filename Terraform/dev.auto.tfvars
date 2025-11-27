d30057dec86ab467007c5135499c2bbf79409a5f
DHRUB DAS <dhrub27das@gmail.com>
2025-11-27 11:51:26 +0530

    vpc infra created using terraform

--- Terraform/dev.auto.tfvars
+++ Terraform/dev.auto.tfvars
@@ -1,4 +1,5 @@
-env              = "dev"
-cidr-block       = "10.0.0.0/16"
-pub-subnet-count = 4
-pub-cidr-block   = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.64.0/20"]
+env                   = "dev"
+cidr-block            = "10.0.0.0/16"
+pub-subnet-count      = 4
+pub-cidr-block        = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.64.0/20"]
+pub-availability-zone = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -39,3 +39,50 @@ resource "aws_subnet" "public-subnet" {
   depends_on = [aws_vpc.vpc]
 }
 
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${local.org}-${local.project}-${local.env}-public-route-table"
+    Env  = var.env
+  }
+}
+
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = var.pub-subnet-count
+  subnet_id      = element(aws_subnet.public-subnet.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
+
+  depends_on = [aws_route_table.public-rt]
+}
+
+resource "aws_security_group" "eks-cluster-sg" {
+  name        = "${local.org}-${local.project}-${local.env}-sg"
+  description = "Default Security Group"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] // It should be specific IP range
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.org}-${local.project}-${local.env}-sg"
+  }
+}
+
+
+
